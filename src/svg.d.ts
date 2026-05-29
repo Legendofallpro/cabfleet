bd56185e64fa2eae452a5fa7befb7f932b821ff0
CabFleet contributors <85796039+Legendofallpro@users.noreply.github.com>
2026-05-29 14:28:50 +0530

    src/svg.d.ts fix string to SVGR react component type

--- src/svg.d.ts
+++ src/svg.d.ts
@@ -1,9 +1,10 @@
 // svg.d.ts
+// @svgr/webpack (configured in next.config.ts) transforms every .svg import
+// into a React component. The default export is the component, not a URL string.
 declare module "*.svg" {
   import * as React from "react";
-  export const ReactComponent: React.FunctionComponent<
+  const ReactComponent: React.FunctionComponent<
     React.SVGProps<SVGSVGElement> & { title?: string }
   >;
-  const src: string;
-  export default src;
+  export default ReactComponent;
 }
